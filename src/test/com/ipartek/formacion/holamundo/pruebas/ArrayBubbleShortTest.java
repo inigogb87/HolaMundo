cf11c037b87c9cb53463cf0bec5407a59b17f78e
Curso <Curso@GBRB14.ipartekaula.com>
2015-12-03 13:28:52 +0100

    declarar un arrray de 6 elementos con bubleshort

---
+++ src/test/com/ipartek/formacion/holamundo/pruebas/ArrayBubbleShortTest.java
@@ -0,0 +1,21 @@
+package com.ipartek.formacion.holamundo.pruebas;
+
+public class ArrayBubbleShortTest {
+
+	public static void main(String[] args) {
+		int[] miArray = new int[] {5,10,3,0,7,-3};
+		for (int i=0;i<miArray.length;i++)
+			  System.out.println(miArray[i]);
+		
+	
+
+	 
+      
+	}
+
+	private static void ArrayBubbleShortTest(int[] miArray) {
+		
+		ArrayBubbleShortTest(miArray);
+		  System.out.println("");
+	}
+}
